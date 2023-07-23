9585237f474b1414d6f855bfcbe73587195d7f70
Daniel Falk <daniel.falk.1@fixedit.ai>
2023-07-23 15:29:32 +0200

    Update simple examples
    - Log to both stdout and syslog
    - Use binary name from manifest file

--- check-glibc-version/app/src/check_glibc.c
+++ check-glibc-version/app/src/check_glibc.c
@@ -1,21 +1,43 @@
+#include <stdarg.h>
 #include <stdio.h>
+#include <syslog.h>
 #ifdef __GLIBC__
 #include <gnu/libc-version.h>
 #endif
 #include <glib.h>
 
+/* Log a message both to the system logger and to stdout */
+void logmsg(int priority, const char *fmt, ...) {
+  va_list args;
+
+  /* Log to stdout */
+  va_start(args, fmt);
+  vprintf(fmt, args);
+  printf("\n");
+  va_end(args);
+
+  /* Log to system logger */
+  va_start(args, fmt);
+  vsyslog(priority, fmt, args);
+  va_end(args);
+}
+
 int main(void) {
-  printf("GLIB compile-time version:\t%d.%d.%d\n", GLIB_MAJOR_VERSION,
+  openlog(APP_NAME, LOG_PID | LOG_CONS, LOG_USER);
+
+  logmsg(LOG_INFO, "GLIB compile-time version:\t%d.%d.%d", GLIB_MAJOR_VERSION,
          GLIB_MINOR_VERSION, GLIB_MICRO_VERSION);
-  printf("GLIB runtime version:     \t%d.%d.%d\n", glib_major_version,
+  logmsg(LOG_INFO, "GLIB runtime version:     \t%d.%d.%d", glib_major_version,
          glib_minor_version, glib_micro_version);
 
 #ifdef __GLIBC__
-  printf("GNU libc compile-time version:\t%u.%u\n", __GLIBC__, __GLIBC_MINOR__);
-  printf("GNU libc runtime version:     \t%s\n", gnu_get_libc_version());
+  logmsg(LOG_INFO, "GNU libc compile-time version:\t%u.%u", __GLIBC__,
+         __GLIBC_MINOR__);
+  logmsg(LOG_INFO, "GNU libc runtime version:     \t%s",
+         gnu_get_libc_version());
   return 0;
 #else
-  puts("Not the GNU C Library");
+  logmsg(LOG_INFO, "Not the GNU C Library");
   return 1;
 #endif
 }
--- hello_world/app/src/main.c
+++ hello_world/app/src/main.c
@@ -1,6 +1,26 @@
+#include <stdarg.h>
 #include <stdio.h>
+#include <syslog.h>
+
+/* Log a message both to the system logger and to stdout */
+void logmsg(int priority, const char *fmt, ...) {
+  va_list args;
+
+  /* Log to stdout */
+  va_start(args, fmt);
+  vprintf(fmt, args);
+  printf("\n");
+  va_end(args);
+
+  /* Log to system logger */
+  va_start(args, fmt);
+  vsyslog(priority, fmt, args);
+  va_end(args);
+}
 
 int main(void) {
-  printf("Hello, world!\n");
+  openlog(APP_NAME, LOG_PID | LOG_CONS, LOG_USER);
+
+  logmsg(LOG_INFO, "Hello world!\n");
   return 0;
 }
